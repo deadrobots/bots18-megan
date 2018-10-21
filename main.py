0dc87340491e7ffcf7cfaaf6712203e5c23c2525
Megan B <meganbogusz22@gmail.com>
2018-10-21 14:39:00 -0400

    Cleaning up code, new functions, new constants

--- main.py
+++ main.py
@@ -1,52 +1,63 @@
 #!/usr/bin/python
 import os, sys
 from wallaby import *
+thport = 1
+#lm= Left Motor
+lm = 1
+#rm= Right Motor
+rm = 0
+etPort = 0
+dist = 0
+armservo = 0
+clawservo = 1
+
 def drive(leftspeed, rightspeed, time):
-    motor(1, leftspeed)
-    motor(0, rightspeed)
+    motor(lm, leftspeed)
+    motor(rm, rightspeed)
     msleep(time)
     ao()
+
+def grabcan():
+    set_servo_position(clawservo, 1775)
+    msleep(1000)
+    set_servo_position(armservo, 390)
+    msleep(1000)
+    wait_for_button()
+
+def dropcan():
+    set_servo_position(clawservo, 750)
+    msleep(1000)
+    set_servo_position(armservo, 890)
+    msleep(1000)
 
 def main():
     print "Hi there I am Waiting for you to push the button"
     # add tophat"
     wait_for_button()
     enable_servos()
-    set_servo_position(1,1775)
-    msleep (1000)
-    set_servo_position(0,390)
-    msleep (1000)
-    wait_for_button()
-    motor(0,100)
-    motor(1,100)
-    etPort=0
-    dist=0
+
+    drive(100, 100, 100)
+
     while(dist<2600):
         dist=analog(etPort)
         print dist
     ao()
     # thport
-    if analog(thport)>1500
-        #on  black
-    motor(0,50)
-    motor(1,100)
-    else #on white
-    motor(0,100)
-    motor(1,50)
+    if analog(thport)>1500: #on  black
+        drive(100, 50, 10)
+
+    else: #on white
+        drive(50, 100, 10)
     wait_for_button()
-    set_servo_position(1, 750)
-    msleep (1000)
-    set_servo_position(0,890)
-    msleep (1000)
+
     for x in range(0, 4):
         drive(100, 100, 1000)
         drive(100, 0, 980)
-    set_servo_position(1, 2047)
+    set_servo_position(clawservo, 2047) #opens up claw all the way to grab can
     msleep(1000)
 
     # Stop
-    motor(1, 0)
-    motor(0, 0)
+    ao()
     msleep(1000)
 
 def wait_for_button():
